d0facf01a93658cb611b277e99b5c3828ba13af6
gauravdravida <gdravida@udel.edu>
2019-10-21 22:11:35 -0400

    final project

--- views.py
+++ views.py
@@ -1,46 +1,28 @@
 from route_helper import simple_route
-
-GAME_HEADER = """
-<h1>Welcome to adventure quest!</h1>
-<p>At any time you can <a href='/reset/'>reset</a> your game.</p>
-"""
+from flask import Flask, render_template
+from flask import request
 
 
 @simple_route('/')
-def hello(world: dict) -> str:
+def index(world: dict) -> str:
     """
     The welcome screen for the game.
 
     :param world: The current world
     :return: The HTML to show the player
     """
-    return GAME_HEADER+"""You are in the Lair of the Corgis.<br>
-    
-    <a href="goto/lair">Go further into the lair.</a><br>
-    <a href="goto/entrance">Retreat.</a>"""
+    return render_template("index.html")
 
 
-ENCOUNTER_MONSTER = """
-<!-- Curly braces let us inject values into the string -->
-You are in {}. You found a monster!<br>
 
-<!-- Image taken from site that generates random Corgi pictures-->
-<img src="http://placecorgi.com/260/180" /><br>
-    
-What is its name?
-
-<!-- Form allows you to have more text entry -->    
-<form action="/save/name/">
-    <input type="text" name="player"><br>
-    <input type="submit" value="Submit"><br>
-</form>
-"""
-
-
+def INITIATION(location):
+    return render_template(f"{location}.html")
+#def GAME_HEADER():
+#    return render_template("FirstObstacle.html")
 @simple_route('/goto/<where>/')
 def open_door(world: dict, where: str) -> str:
     """
-    Update the player location and encounter a monster, prompting the player
+    Tell the user they are spiderman and prompt the player
     to give them a name.
 
     :param world: The current world
@@ -48,21 +30,33 @@ def open_door(world: dict, where: str) -> str:
     :return: The HTML to show the player
     """
     world['location'] = where
-    return GAME_HEADER+ENCOUNTER_MONSTER.format(where)
+    return INITIATION(world['location'])
 
-
+#@simple_route('/goto/')
 @simple_route("/save/name/")
-def save_name(world: dict, monsters_name: str) -> str:
+def save_name(world: dict, Spider_name: str) -> str:
     """
-    Update the name of the monster.
+    Update the name of Spiderman.
 
     :param world: The current world
     :param monsters_name:
     :return:
     """
-    world['name'] = monsters_name
+    world['name'] = Spider_name
 
-    return GAME_HEADER+"""You are in {where}, and you are nearby {monster_name}
-    <br><br>
-    <a href='/'>Return to the start</a>
-    """.format(where=world['location'], monster_name=world['name'])
+    return render_template("FirstObstacle.html", name=Spider_name)
+
+@simple_route('/yes/')
+def answer(world: dict):
+    return render_template("answer.html")
+@simple_route('/no/')
+def answer2(world: dict):
+    return render_template("No.html")\
+
+
+@simple_route('/move/', methods=["POST"])
+def move(world:dict, *args):
+    if request.values["move"]=="water":
+        return render_template("waterhim.html")
+    else:
+        return render_template("webs.html")
